ab7ec084f08edcbfb3396d508021ed1ae7bb461a
xuanheng <xuanheng@pinduoudo.com>
2019-04-13 04:16:54 +0800

    update post

--- source/build.sh
+++ source/build.sh
@@ -2,10 +2,8 @@
 cd /Users/yuedong/Blog/source
 rm -rf _site
 jekyll b
-mv _site/* ../
 cd /Users/yuedong/Blog/
 ls | grep -v source | xargs rm -rf
+mv source/_site/* ../
 git add . && git commit -am 'update post'
 git push
-
-
