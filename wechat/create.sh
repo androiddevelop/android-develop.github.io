8e2c48cc83162f367b1feca95532db6db60221e5
xuanheng <xuanheng@pinduoudo.com>
2019-07-08 01:17:34 +0800

    update post

--- wechat/create.sh
+++ wechat/create.sh
@@ -7,7 +7,7 @@ mkdir content
 position=1;
 for loop in `ls ../_posts`
 do
- id=${loop/.md/};
+ id=${loop/.md};
  echo "$id $position";
  content=`cat template`;
  content=${content/cb_page_index/$position}
