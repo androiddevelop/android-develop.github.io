20090d35a047892cdb16e79789c0696b83aad915
xuanheng <xuanheng@pinduoudo.com>
2019-11-21 11:45:47 +0800

    update post

---
+++ wechat/create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf content
+mkdir content
+
+# 生成文件
+position=1;
+for loop in `ls ../_posts`
+do
+ id=${loop/.md};
+ echo "$id $position";
+ content=`cat template`;
+ content=${content/cb_page_index/$position}
+ echo "${content}" > "content/$id"
+ ((position++));
+done
